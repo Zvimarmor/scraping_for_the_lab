4e2a431d5504db5eea2be160d2ac2a0ee9f82ebf
Zvi Marmor <Zvi.marmor@mail.huji.ac.il>
2024-09-26 13:26:02 +0300

    the scraping itself works!!

--- scraping.py
+++ scraping.py
@@ -12,68 +12,40 @@ import warnings
 import multiprocessing as mp
 import time
 
-
-
-def get_trfs_list(file_path):
+def get_trfs_set(file_path):
     """
     Get the list of tRFs from the given file.
     :param file_path: The path to the file containing the tRFs.
     :return: A set of tRFs.
     """
-    trfs = set()
-
     # Check if the file exists first
     if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            current_line = f.readlines()
-            current_line = [x.strip() for x in current_line]  # Strip whitespace from each line
-
-            # Split the lines into words and check for 'tRF' in each word
-            for line in current_line:
-                words = line.split()
-                for word in words:
-                    if 'tRF' in word and word[0:3] == 'tRF' and len(word) > 5:
-                        word.replace("'", "")
-                        word.replace(" ", "")
-                        word.replace('"', "")
-                        trfs.add(word)
+        trfs = pd.read_csv(file_path, header=None)
+        trfs = set(trfs.iloc[:,0])
         return trfs
 
     else:
-        warnings.warn('The file does not exist. Please check the file path.')
+        warnings.warn('The trf file does not exist. Please check the file path.')
         return None
 
-def get_miRNA_list(file_path):
+def get_miRNA_set(file_path):
     """
     Get the list of miRNAs from the given file.
     :param file_path: The path to the file containing the miRNAs.
     :return: A set of miRNAs.
     """
-    mirnas = set()
 
     # Check if the file exists first
     if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            current_line = f.readlines()
-            current_line = [x.strip() for x in current_line]  # Strip whitespace from each line
-
-            # Split the lines into words and check for 'miR' in each word
-            for line in current_line:
-                words = line.split()
-                for word in words:
-                    if 'miR' in word and len(word) > 5 and word[4:7] == 'miR':
-                        word.replace("'", "")
-                        word.replace(" ", "")
-                        word.replace('"', "")
-                        mirnas.add(word)
-        return mirnas
+        miRNAs = pd.read_csv(file_path, header=None)
+        miRNAs = set(miRNAs.iloc[:,0])
+        return miRNAs
 
     else:
-        warnings.warn('The file does not exist. Please check the file path.')
+        warnings.warn('The miRNA file does not exist. Please check the file path.')
         return None
     
-
-def check_genes_for_tRFs(tRFs,output_file, if_save=False):
+def check_genes_for_tRFs(tRFs,output_file='conclusion_TRFs_genes', if_save=False):
     """
     Checks the genes for the given tRFs.
     :param tRFs: A set of tRFs to check.
@@ -83,6 +55,8 @@ def check_genes_for_tRFs(tRFs,output_file, if_save=False):
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=chrome_options)
     driver.get('http://www.rnanut.net/tRFTar/')
     driver.implicitly_wait(10)
@@ -118,80 +92,135 @@ def check_genes_for_tRFs(tRFs,output_file, if_save=False):
             driver.switch_to.window(window)
             break
 
-    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[3]/div[1]/span[2]/span/button/span[1]').click()
-    driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[3]/div[1]/span[2]/span/div/a[4]').click()
-
-    page = 2
-    number_of_pages = driver.find_element(By.CLASS_NAME, 'pagination-info').text.split()[-2]
-    number_of_pages = (int(number_of_pages)//100 if int(number_of_pages) % 100 == 0 else int(number_of_pages)//100 + 1)
-    for _ in range(number_of_pages):
-        for i in range(1,105):
-            try:
-                search_results = driver.find_element(By.XPATH, '//*[@id="search-table"]/tbody/tr[' + str(i) + ']/td[1]/a')
-                if 'tRF' in search_results.text:
-                    trf = search_results.text
-                    if trf not in genes_dict:
-                        genes_dict[trf] = []
-                    search_results = driver.find_element(By.XPATH, '//*[@id="search-table"]/tbody/tr[' + str(i) + ']/td[3]/a')
-                    gene = search_results.text
-                    search_results = driver.find_element(By.XPATH, '//*[@id="search-table"]/tbody/tr[' + str(i) + ']/td[6]')
-                    score = search_results.text
-                    genes_dict[trf].append((gene, score))
-
-            except:
-                break
-        page += 1
+    try:
+        driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[3]/div[1]/span[2]/span/button/span[1]').click()
+        driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[3]/div[1]/span[2]/span/div/a[4]').click()
         time.sleep(2)
-        driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[3]/div[2]/ul/li[' + str(page) + ']/a').click()  
+        page = 2
+        number_of_pages = driver.find_element(By.CLASS_NAME, 'pagination-info').text.split()[-2]
+        number_of_pages = (int(number_of_pages)//100 if int(number_of_pages) % 100 == 0 else int(number_of_pages)//100 + 1)
+        for _ in range(number_of_pages):
+            for i in range(1, 105):
+                try:
+                    search_results = driver.find_element(By.XPATH, '//*[@id="search-table"]/tbody/tr[' + str(i) + ']/td[1]/a')
+                    if 'tRF' in search_results.text:
+                        trf = search_results.text
+                        if trf not in genes_dict:
+                            genes_dict[trf] = []
+                        search_results = driver.find_element(By.XPATH, '//*[@id="search-table"]/tbody/tr[' + str(i) + ']/td[3]/a')
+                        gene = search_results.text
+                        search_results = driver.find_element(By.XPATH, '//*[@id="search-table"]/tbody/tr[' + str(i) + ']/td[6]')
+                        score = search_results.text
+                        genes_dict[trf].append((gene, score))
+
+                except:
+                    break
+            page += 1
+            driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[3]/div[2]/ul/li[' + str(page) + ']/a').click()
+            time.sleep(2)
     
+    except:
+        page = 1
+        number_of_pages = 1
+        search_results = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[2]/div[2]/table/tbody/tr/td[1]/a')
+        if 'tRF' in search_results.text:
+            trf = search_results.text
+            if trf not in genes_dict:
+                genes_dict[trf] = []
+            search_results = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[2]/div[2]/table/tbody/tr/td[3]/a')
+            gene = search_results.text
+            search_results = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/div[2]/div[2]/table/tbody/tr/td[6]')
+            score = search_results.text
+            genes_dict[trf].append((gene, score)) 
+    
+    ### Save the results to a file if needed ###
     if if_save:
-        with open(output_file, 'w') as f:
-            f.write('tRF gene score' + '\n')
-            for trf in genes_dict:
-                for gene in genes_dict[trf]:
-                    f.write(trf + ' ' + gene[0] + ' ' + gene[1] + '\n')
+        df = pd.DataFrame(columns=['tRF', 'gene', 'score'])
+        for trf in genes_dict:
+            for gene in genes_dict[trf]:
+                new_row = {'tRF': trf, 'gene': gene[0], 'score': gene[1]}
+                df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+        df.to_csv(output_file, index=False)
 
     return genes_dict
 
-def check_genes_for_miRNAs(miRNAs, output_file, if_save=False):
+def check_genes_for_miRNAs(miRNAs, output_file='conclusion_miRNAs_genes', if_save=False, min_score=0.8, min_sources=10):
     """
-    Checks the genes for the given miRNAs.
-    :param miRNAs: A set of miRNAs to check.
-    :return: A list of genes.
+    Checks the genes for the given miRNAs, using the mirDIP website.
+    :param miRNAs: A list of miRNAs to check.
+    :return: A list of genes. 
     """
     genes_dict = {}
 
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
-    # driver = webdriver.Chrome(options=chrome_options)
-    driver = webdriver.Chrome()
-    driver.get('https://google.com')
-    # driver.get('https://ophid.utoronto.ca/mirDIP/index.jsp#r')
+    ### Set up the web driver and go to the website ###
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('https://ophid.utoronto.ca/mirDIP/index.jsp#r')
     driver.implicitly_wait(10)
-
-    print('driver.current_url)')
 
     search_box = driver.find_element(By.ID, 'idMicroRna')
     search_box.send_keys(','.join(miRNAs))
-    driver.find_element(By.ID, 'submitbutton').click()
+    driver.find_element(By.XPATH,'/html/body/div[2]/div/form/div[3]/table/tbody/tr/td/div[1]/input[3]').click()
 
+    ### Download the file and save it as a csv file ###
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    prefs = {"download.default_directory": current_dir} #set the download directory. CHANGE THIS TO YOUR DESIRED DIRECTORY
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    before_download = set(os.listdir(current_dir))
+    driver.find_element(By.XPATH,'/html/body/div[2]/div/form/div[3]/table/tbody/tr/td/div[2]/input[2]').click()
+    driver.find_element(By.XPATH,'/html/body/div[2]/div/form/div[3]/table/tbody/tr/td/div[1]/input[3]').click() #download the txt file
+    time.sleep(5)
+    after_download = set(os.listdir(current_dir))
+
+    new_files = after_download - before_download
+    downloaded_file = new_files.pop()
+    target_name = "mirDIP_result.txt"  # Rename the file to a new name
+    os.rename(os.path.join(current_dir, downloaded_file), os.path.join(current_dir, target_name))
+    lines_to_skip = 1
+    for line in open(target_name):
+        if line.startswith('Results'):
+            break
+        lines_to_skip += 1
+    df = pd.read_csv(target_name, sep='\t', skiprows= lines_to_skip)
+    df.to_csv('mirDIP_result.csv', index=False)
+
+    ### Read the csv file and get the genes for each miRNA after filtering ###
+    df = pd.read_csv('mirDIP_result.csv')
+    df = df[df['Integrated Score']>=min_score]
+    df = df[df['Number of Sources'] >= min_sources]
+
+    for index, row in df.iterrows():
+        miRNA = row['MicroRNA']
+        gene = row['Gene Symbol']
+        score = row['Integrated Score']
+        if miRNA not in genes_dict:
+            genes_dict[miRNA] = []
+        genes_dict[miRNA].append((gene, score))
+    os.remove(target_name)
+    os.remove('mirDIP_result.csv')
+
+    ### Save the results to a file if needed ###
     if if_save:
-        with open(output_file, 'w') as f:
-            f.write('miRNA gene' + '\n')
-            for miRna in genes_dict:
-                for gene in genes_dict[miRna]:
-                    f.write(miRna + ' ' + gene + '\n')
+        df = pd.DataFrame(columns=['miRNA', 'gene', 'score'])
+        for miRNA in genes_dict:
+            for gene in genes_dict[miRNA]:
+                new_row = {'miRNA': miRNA, 'gene': gene[0], 'score': gene[1]}
+                df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+        df.to_csv(output_file + '.csv', index=False)
+
+    return genes_dict
 
 if __name__ == '__main__':
-    trf_list = get_trfs_list('conclusion_TRFs.txt')
-    check_genes_for_tRFs(trf_list, 'conclusion_TRFs_genes.txt', if_save=True)
-    # mirna_list = get_miRNA_list('conclusion_miRNA.txt')
-    # for mirna in mirna_list:
-    #     print(mirna)
-    #print(check_genes_for_miRNAs(mirna_list))
-   
-
-
+    trf_list = get_trfs_set('tRF_AD_Meitar_030924.csv')
+    check_genes_for_tRFs(trf_list, 'conclusion_TRFs_genes.csv', if_save=True)
+    mirna_list = ['hsa-miR-125a-5p', 'hsa-miR-146a', 'hsa-miR-150-5p', 'hsa-miR-191-5p', 'hsa-miR-199a-3p', 'hsa-miR-200a', 'hsa-miR-203a-3p', 'hsa-miR-223-3p', 'hsa-miR-372', 'hsa-miR-375', 'hsa-miR-499a-5p', 'hsa-miR-663b', 'hsa-miR-885-5p']
+    check_genes_for_miRNAs(mirna_list, if_save=True)
+        
 
         
 
